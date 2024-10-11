8de912c0b35416cc6f15d82a7e075376220118e3
Nishant CM <cmnishantwork@gmail.com>
2024-10-11 23:55:04 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+function App () {
+    return (
+        <main>
+            <div>
+                <h1>Nishant CM</h1>
+            </div>
+        </main>
+    );
+}
+
+export default App
