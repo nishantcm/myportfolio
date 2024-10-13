c1131f1473645e94b6e66feada4c202531622837
Nishant CM <cmnishantwork@gmail.com>
2024-10-14 00:00:01 +0530

    Add header.jsx file

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header ( section ) {
+    return (
+        <header>
+            <div className="card">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Resume</li>
+                    <li>Services</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>My Blogs</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </header>
+    );
+}
