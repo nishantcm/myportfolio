3c50bb2a4af23b628b5ddc4ff8fe12c0b0f3f14e
Nishant CM <cmnishantwork@gmail.com>
2024-10-14 00:11:45 +0530

    Update the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import { useState } from "react";
+import Header from "./Header";
 
 function App () {
     return (
         <main>
             <div>
                 <h1>Nishant CM</h1>
+            </div>
+            <div>
+                <Header section />
             </div>
         </main>
     );
