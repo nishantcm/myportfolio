13a9afd121b03238dc6c70cc80e73560a0a4eefa
Nishant CM <cmnishantwork@gmail.com>
2024-10-14 02:04:53 +0530

    Update the App.jsx and Header.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 // import { useState } from "react";
 import Header from "./Header";
+<script
+  src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+  crossorigin></script>
 
 function App () {
     return (
         <main>
             <div>
                 <h1>Nishant CM</h1>
+                <div className="d-flex justify-content-between">
+                    <Header section />
+                </div>
+                
             </div>
-            <div>
-                <Header section />
-            </div>
+                
         </main>
     );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,37 @@
-export default function Header ( section ) {
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+
+export default function Header( section ) {
     return (
-        <header>
-            <div className="card">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Resume</li>
-                    <li>Services</li>
-                    <li>Skills</li>
-                    <li>Projects</li>
-                    <li>My Blogs</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        </header>
+        <Card>
+            <Card.Header>
+                <Nav className="flex-row" defaultActiveKey="#first">
+                    <Nav.Item>
+                        <Nav.Link href="#first">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Resume</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Services</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Skills</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Projects</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">My Blogs</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#">Contact</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Card.Header>
+        </Card>
     );
 }
