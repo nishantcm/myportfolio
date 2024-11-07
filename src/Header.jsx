a4c8f3469f632a736d4397adfb19ac4b5157d119
Nishant CM <cmnishantwork@gmail.com>
2024-11-07 23:57:36 +0530

    Update the Header.jsx file

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,39 +1,39 @@
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
+import { Container, Navbar } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 // import Stack from "react-bootstrap/Stack";
 
 export default function Header() {
     return (
-        <Card>
-            <Card.Header>
-                    <Nav defaultActiveKey="#first">
-                        <Nav.Item>
-                            <Nav.Link href="#first">Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">About</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">Resume</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">Services</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">Skills</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">Projects</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">My Blogs</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#">Contact</Nav.Link>
-                        </Nav.Item>
+        <>
+            <Navbar defaultActiveKey="#first">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#first">Home</Nav.Link>
+
+                        <Nav.Link href="#">About</Nav.Link>
+
+                        <Nav.Link href="#">Resume</Nav.Link>
+
+                        <Nav.Link href="#">Services</Nav.Link>
+
+                        <Nav.Link href="#">Skills</Nav.Link>
+
+                        <Nav.Link href="#">Projects</Nav.Link>
+
+                        <Nav.Link href="#">My Blogs</Nav.Link>
+
+                        <Nav.Link href="#">Contact</Nav.Link>
                     </Nav>
+                    
+                </Container>
+            </Navbar>
+        </>
+        // <Card>
+        //     <Card.Header>
                 
-            </Card.Header>
-        </Card>
+                
+        //     </Card.Header>
+        // </Card>
     );
 }
