cbb4a69c84a7a43ccf53d2a144db4146d12b7a8b
Nishant CM <cmnishantwork@gmail.com>
2024-11-10 23:55:56 +0530

    Update the file

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 // import Card from "react-bootstrap/Card";
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Row } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import Col from "react-bootstrap/Col";
 // import Stack from "react-bootstrap/Stack";
 
 export default function Header() {
@@ -8,26 +9,27 @@ export default function Header() {
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container className="navStyle">
-                    <Navbar.Brand href="#home">
-                        <h1>Nishant CM</h1>
-                    </Navbar.Brand>
-                    <Nav className="">
-                        <Nav.Link href="#home">Home</Nav.Link>
-
-                        <Nav.Link href="#">About</Nav.Link>
-
-                        <Nav.Link href="#">Resume</Nav.Link>
-
-                        <Nav.Link href="#">Services</Nav.Link>
-
-                        <Nav.Link href="#">Skills</Nav.Link>
-
-                        <Nav.Link href="#">Projects</Nav.Link>
-
-                        <Nav.Link href="#">My Blogs</Nav.Link>
-
-                        <Nav.Link href="#">Contact</Nav.Link>
-                    </Nav>
+                    <Row>
+                        <Col md={3}>
+                            <Navbar.Brand href="#home">
+                                <h1>Nishant CM</h1>
+                            </Navbar.Brand>
+                        </Col>
+                        <Col md={9}>
+                            <Nav className="">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#">About</Nav.Link>
+                                <Nav.Link href="#">Resume</Nav.Link>
+                                <Nav.Link href="#">Services</Nav.Link>
+                                <Nav.Link href="#">Skills</Nav.Link>
+                                <Nav.Link href="#">Projects</Nav.Link>
+                                <Nav.Link href="#">My Blogs</Nav.Link>
+                                <Nav.Link href="#">Contact</Nav.Link>
+                            </Nav>
+                        </Col>
+                    </Row>
+                    
+                    
                 </Container>
             </Navbar>
         </>
