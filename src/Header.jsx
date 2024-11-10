bd6aaca06711be94549fc28a00644081b74e980b
Nishant CM <cmnishantwork@gmail.com>
2024-11-11 01:59:04 +0530

    Update the header file

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 // import Card from "react-bootstrap/Card";
-import { Container, Navbar, Row } from "react-bootstrap";
+import { Navbar, Row } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Col from "react-bootstrap/Col";
 // import Stack from "react-bootstrap/Stack";
@@ -7,30 +7,26 @@ import Col from "react-bootstrap/Col";
 export default function Header() {
     return (
         <>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container className="navStyle">
-                    <Row>
-                        <Col md={3}>
-                            <Navbar.Brand href="#home">
-                                <h1>Nishant CM</h1>
-                            </Navbar.Brand>
-                        </Col>
-                        <Col md={9}>
-                            <Nav className="">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#">About</Nav.Link>
-                                <Nav.Link href="#">Resume</Nav.Link>
-                                <Nav.Link href="#">Services</Nav.Link>
-                                <Nav.Link href="#">Skills</Nav.Link>
-                                <Nav.Link href="#">Projects</Nav.Link>
-                                <Nav.Link href="#">My Blogs</Nav.Link>
-                                <Nav.Link href="#">Contact</Nav.Link>
-                            </Nav>
-                        </Col>
-                    </Row>
-                    
-                    
-                </Container>
+            <Navbar>
+                <Row>
+                    <Col md={3}>
+                        <Navbar.Brand href="#home">
+                            <h1 className="headerColor">Nishant CM</h1>
+                        </Navbar.Brand>
+                    </Col>
+                    <Col md={9}>
+                        <Nav className="justify-content-end">
+                            <Nav.Link href="#" className="headerColor">Home</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">About</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">Resume</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">Services</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">Skills</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">Projects</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">My Blogs</Nav.Link>
+                            <Nav.Link href="#" className="headerColor">Contact</Nav.Link>
+                        </Nav>
+                    </Col>
+                </Row>
             </Navbar>
         </>
         // <Card>
