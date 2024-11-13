2387ea72489e065fa367becee4752d3d59667112
Nishant CM <cmnishantwork@gmail.com>
2024-11-13 22:41:59 +0530

    Update the file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 <script
   src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
-  crossorigin></script>
+  crossorigin>
+</script>
 
 function App () {
     return (
