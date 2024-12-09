a96856b0b9cdd29f4a13ebbccd7058dd8b9b3fdb
Nishant CM <cmnishantwork@gmail.com>
2024-12-09 22:11:18 +0530

    Update the Home file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,24 @@ import Container from "react-bootstrap/Container";
 
 export default function Home() {
     return (
-        
+        <Container>
+            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        {/* <img src={} className="d-block w-100"></img> */}
+                        <div className="carousel-caption">
+                            <h6>Hello !</h6>
+                            <h1>I'm Nishant CM</h1>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        {/* <img src={} className="d-block w-100"></img> */}
+                    </div>
+                    <div className="carousel-item">
+                        {/* <img src={} className="d-block w-100"></img> */}
+                    </div>
+                </div>
+            </div>
+        </Container>
     );
 }
